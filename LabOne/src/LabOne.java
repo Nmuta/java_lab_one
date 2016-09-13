a587d792a4e2dd74f44c85b81a676101e95bfc4e
Nmuta Jones <nmuta1@gmail.com>
2016-09-13 13:38:09 -0600

    Initial files

---
+++ LabOne/src/LabOne.java
@@ -0,0 +1,32 @@
+
+public class LabOne {
+
+	public static void main(String[] args) {
+		
+		System.out.println("It works!"); 
+		
+		int addition = AddStuff(5,6); 
+		System.out.println(addition); 
+
+	}
+	
+	public static int AddStuff(int num, int num2){
+		int result = num + num2;
+		return result;
+	}
+	
+
+	
+	//  TODO: 
+	// add a static method to concatenate two strings
+	// add a static method to swap two elements in an array
+	// add a static method to reverse a string
+	// add a static tip calculator method
+	// think about this:  what the h*** does static mean?  
+	
+	// call all of these methods from the main class && use System.out to print the values
+	
+	
+	
+
+}
